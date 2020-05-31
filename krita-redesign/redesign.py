49681f42a0af4c5db4c611d7c2138e940fdb083e
Kapyia <kapyia.art@gmail.com>
2020-05-31 19:24:25 +0200

    Current status of "rmWhiteLines"
    Since I created the original extension I have made a couple of additions to it: the transparent toolbox as well as CSS to shrink the document tabs. The latter however wasn't useful since I wasn't able to consistently grab the specific tab bar.

--- krita-redesign/redesign.py
+++ krita-redesign/redesign.py
@@ -7,12 +7,203 @@ class Redesign(Extension):
 
     def setup(self):
         pass
+        # This function could possibly be used to import a CSS file  
 
     def createActions(self, window):
-        window.qwindow().setStyleSheet("""
-            QToolBar {
-                border: none;
+        actions = []
+        actions.append(window.createAction("toolbarBorder", "Borderless toolbars", ""))
+        actions.append(window.createAction("nuToolbox", "Transparent toolbox", ""))
+        # actions.append(window.createAction("tabHeight", "Thin document tabs (requires window resize)", "window"))
+        menu = window.qwindow().menuBar().addMenu("Redesign")
+
+        for a in actions:
+            a.setCheckable(True)
+            a.setChecked(True) # Activate the tweaks by default
+            menu.addAction(a)
+        
+        actions[0].toggled.connect(self.toolbarBorderToggled)
+        actions[1].toggled.connect(self.nuToolboxToggled)
+        # actions[2].toggled.connect(self.tabHeightToggled)
+
+        styleSheet = """
+                QToolBar {
+                    border: none;
+                }            
+
+                QTabBar::tab:!selected {
+                    color: gray;
+                }
+
+                QTabBar::tab:!selected:hover {
+                    color: silver;
+                }
+            """
+
+        window.qwindow().setStyleSheet(styleSheet)
+        
+        # styleSheet = """
+        #         QTabBar::tab { height: 23px; }
+        #     """
+        # barList = window.qwindow().findChildren(QTabBar)
+
+        # tabBar = # ***The method I used to locate the correct tab bar wasn't consistent***
+
+        # tabBar.setStyleSheet(styleSheet)
+
+        styleSheet = """
+
+            KoToolBoxDocker { 
+                background-color: rgba(128, 128, 128, .01);
+                margin: 2px; 
             }
-        """)
+            
+            .KoToolBoxScrollArea { 
+                background-color: #00000000;
+            }
+            
+            KoToolBoxScrollArea * { 
+                background-color: #00000000;
+            }
+            
+            KoToolBoxDocker QLabel {
+                border: none;
+                border-radius: 4px; 
+                background-color: #77000000;
+            }
+            
+            KoToolBoxScrollArea QToolTip {
+                background-color: #ffffff;                           
+            }
+            
+            KoToolBoxButton {
+                background-color: #77000000;
+                border: none;
+                border-radius: 4px;
+                margin-right: 1px;
+                margin-top: 1px;
+            }
+            
+            KoToolBoxButton:checked {
+                background-color: #aa306fa8;
+            }
+            
+            KoToolBoxButton:hover {
+                background-color: #1c1c1c;
+            }
+            
+            KoToolBoxButton:pressed {
+                background-color: #53728e;
+            }
+            
+        """
+
+        toolbox = window.qwindow().findChild(QWidget, 'ToolBox')
+        
+        # Hides the handle at the top of the toolbox. It can still be manipulated though.
+        # Maybe it's a title bar that can be disabled instead?
+        # handle = toolbox.findChild(QLabel)
+        # handle.setVisible(False) 
+        
+        # Lock the size of the toolbox. Not necessary, just my preference.
+        toolbox.setFixedWidth(58) 
+        toolbox.setFixedHeight(549)
+        
+        toolbox.setStyleSheet(styleSheet)
+
+
+    """ Becuse Krita loads the main window in a very... curious way 
+    the functions below can't be used in 'createActions()'. This is
+    why the code of the functions are ran separately in 'createActions()'.
+    We need to use the 'window' argument of 'createActions()' to
+    set the style sheets immediately on launch. I'm not sure if theres
+    a way to maybe pass a 'window' argument to these function?"""
+
+
+    def toolbarBorderToggled(self, toggled):
+        styleSheet = """""" # Cleared by default
+
+        if toggled:
+            styleSheet = """
+                QToolBar { border: none; }            
+            """
+
+        Application.activeWindow().qwindow().setStyleSheet(styleSheet)
+
+
+    # def tabHeightToggled(self, toggled):
+    #     styleSheet = """""" # Clear by default
+
+    #     if toggled:
+    #         styleSheet = """
+    #             QTabBar::tab { height: 23px; }
+    #         """
+    #     barList = Application.activeWindow().qwindow().findChildren(QTabBar)
+    #     tabBar = # ***The method I used to locate the correct tab bar wasn't consistent***
+    #     tabBar.setStyleSheet(styleSheet)
+
+
+    def nuToolboxToggled(self, toggled):
+        styleSheet = """""" # Clear by default
+
+        if toggled:
+            styleSheet = """
+
+            KoToolBoxDocker { 
+                background-color: rgba(128, 128, 128, .01);
+                margin: 2px; 
+            }
+            
+            .KoToolBoxScrollArea { 
+                background-color: rgba(0,0,0,0);
+            }
+            
+            KoToolBoxScrollArea * { 
+                background-color: rgba(0,0,0,0);
+            }
+            
+            KoToolBoxDocker QLabel {
+                border: none;
+                border-radius: 3px; 
+                background-color: #66000000;
+            }
+            
+            KoToolBoxScrollArea QToolTip {
+                background-color: #ffffff;                           
+            }
+            
+            KoToolBoxButton {
+                background-color: #66000000;
+                border: none;
+                border-radius: 3px;
+                margin-right: 1px;
+                margin-top: 1px;
+            }
+            
+            KoToolBoxButton:checked {
+                background-color: #aa306fa8;
+            }
+            
+            KoToolBoxButton:hover {
+                background-color: inherit;
+            }
+            
+            KoToolBoxButton:pressed {
+                background-color: #53728e;
+            }
+            
+        """
+
+        toolbox = Application.activeWindow().qwindow().findChild(QWidget, 'ToolBox')
+
+         # Hides the handle at the top of the toolbox. It can still be manipulated though.
+        # Maybe it's a title bar that can be disabled instead?
+        # handle = toolbox.findChild(QLabel)
+        # handle.setVisible(False) 
+        
+        # Lock the size of the toolbox. Not necessary, just my preference.
+        toolbox.setFixedWidth(58) 
+        toolbox.setFixedHeight(549)
+
+        toolbox.setStyleSheet(styleSheet)
         
 Krita.instance().addExtension(Redesign(Krita.instance()))
