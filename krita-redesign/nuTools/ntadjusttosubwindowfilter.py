d1ca1cde30f75828d775add27a1c90370e7d428d
Pedro Reis <pedroreis.ad@protonmail.com>
2020-07-13 16:36:04 +0100

    Added GPL License

--- krita-redesign/nuTools/ntadjusttosubwindowfilter.py
+++ krita-redesign/nuTools/ntadjusttosubwindowfilter.py
@@ -1,3 +1,20 @@
+"""
+    Plugin for Krita UI Redesign, Copyright (C) 2020 Kapyia
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from krita import Krita 
 from PyQt5.QtCore import QObject, QEvent, QPoint
 
